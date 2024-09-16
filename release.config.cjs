c4732a741ae9def8d9b9f3a2e9f7a7fe87b62644
Lazar Karic <16634314+lkaric@users.noreply.github.com>
2024-09-16 18:20:34 +0200

    chore: :technologist: release config branch naming

--- release.config.cjs
+++ release.config.cjs
@@ -2,7 +2,7 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 const releaseConfig = {
-  branches: ['master', { name: 'next', prerelease: true }],
+  branches: ['main', { name: 'next', prerelease: true }],
 };
 
 module.exports = releaseConfig;
